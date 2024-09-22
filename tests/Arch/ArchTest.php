c170750d8921c392e373dc2ee67bd7872b6f0999
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2024-09-22 02:33:59 +0200

    chore: install pest and update dependencies
    require php >=8.2 and nikic/php-parser ^5.2

---
+++ tests/Arch/ArchTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// https://github.com/pestphp/pest/blob/3.x/src/ArchPresets/Php.php
+arch()->preset()->php();
+
+// https://github.com/pestphp/pest/blob/3.x/src/ArchPresets/Security.php
+arch()->preset()->security();
